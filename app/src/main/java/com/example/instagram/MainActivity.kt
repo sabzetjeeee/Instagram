26421f0f6c5bce7bbdb78b4df6a26c1103af685b
sabzetjeeee <74899093+sabzetjeeee@users.noreply.github.com>
2023-03-20 16:05:08 +0200

    Edit name activity and Log in activity

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.instagram
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.example.instagram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ibttnEditName.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivityForResult(intent, 1)
+        }
+
+        binding.bttnLogin.setOnClickListener {
+            if (binding.etLogin.text.toString() == "kunichh" && binding.etPassword.text.toString() == "6497") {
+                binding.llLogin.isVisible = false
+            }
+            else
+                binding.tvWrongData.text = "Невірні данні!"
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+            binding.tvName.text = data?.getStringExtra("username")
     }
 }
---
+++ app/src/main/java/com/example/instagram/SecondActivity.kt
@@ -0,0 +1,24 @@
+package com.example.instagram
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.instagram.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bttnSave.setOnClickListener {
+            var resultIntent = Intent(this, MainActivity::class.java)
+            resultIntent.putExtra("username", binding.etName.text.toString() + " " + binding.etSurname.text.toString())
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        }
+    }
+}
